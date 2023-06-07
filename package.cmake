e50c78d587002ec6242ccf79856ae991d8034570
AR Visions <kalen@krworks.com>
2023-06-07 13:13:50 -0700

    move source in from ion/ci; this repo holds onto an ignored install path and has a peer relationship with other source at its file-system level

---
+++ package.cmake
@@ -0,0 +1,12 @@
+cmake_minimum_required(VERSION 3.20)
+
+if (DEFINED PackageGuard)
+    return()
+endif()
+set(PackageGuard yes)
+
+function(find_import name fields)
+    sbeParseJson(package package_contents)
+endfunction()
+
+
