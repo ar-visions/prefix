eb00797fe5518244721883e3fa2f1aa3f422dbfd
Kalen N White <kalen@krworks.com>
2023-09-20 08:46:34 -0700

    add aarch64 cmake (may make these json)

---
+++ cross/aarch64-linux-gnu.cmake
@@ -0,0 +1,10 @@
+# /usr/local/lib/gcc/aarch64-linux-gnu/12.3.1/
+
+# cmake-toolchain-file/arm-gnu-toolchain.cmake
+set(CMAKE_SYSTEM_NAME                   Linux)
+set(CMAKE_C_COMPILER                    arm-gnu-toolchain-gcc)
+set(CMAKE_CXX_COMPILER                  arm-gnu-toolchain-g++)
+set(CMAKE_FIND_ROOT_PATH                /opt/toolchain/aarch64-linux-gnu)
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM   NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY   ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE   ONLY)
